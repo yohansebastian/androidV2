3693b20aec74ce6c7ef840b3598d33a787b16ea3
yirleyson <sdiazosp@cesde.net>
2021-09-18 17:59:35 -0500

    aplicacion de firebase

--- app/src/main/java/com/example/mobiles2/MainActivity.java
+++ app/src/main/java/com/example/mobiles2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobiles2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,7 +10,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     EditText etEmail, etPassword;
     Button btnLogin,btnTest;
     @Override
@@ -23,6 +33,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnTest.setOnClickListener(this);
         btnLogin.setOnClickListener(this);
+    }
+    public void createUser(){
+        Map<String, Object> userData = new HashMap<>();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        userData.put("email",email);
+        userData.put("pass",password);
+        db.collection("users")
+                .add(userData)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Usuario agregado", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "No se pudo agregar", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
     public void login(){
         String email = etEmail.getText().toString();
@@ -45,7 +76,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Que tenga compatibilidad con distintos tipos de pantalla.
 
-
+    android soporte multiples tamaños de pantalla, buscar eso para
+     que la aplicación sea multipantalla
+     https://material.io/design/material-theming/overview.html#using-material-theming
+     https://material.io/resources/color/#!/?view.left=1&view.right=0&primary.color=4DD0E1&secondary.color=F8BBD0
+     para ver las combinaciones de colores usar la siguiente pagina :
+     https://www.materialpalette.com/lime/yellow
+     https://www.materialpalette.com/icons
     */
     @Override
     public void onClick(View view) {
@@ -54,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 login();
                 break;
             case R.id.btnTest:
-                Toast.makeText(this, "Es el test", Toast.LENGTH_SHORT).show();
+                createUser();
                 break;
         }
     }
